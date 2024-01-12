f8dea5d63fb9debb09968e6c43c02e738c24c75c
sergey <serega@300kbit.ru>
2024-01-12 05:26:47 +0000

    /routes channels, console > /App/Routing/Routes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,8 +22,8 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        //$this->load(__DIR__.'/Commands');
+        $this->load(__DIR__.'/Commands');
 
-        //require base_path('routes/console.php');
+        require app_path('Routing/Routes/Console.php');
     }
 }
